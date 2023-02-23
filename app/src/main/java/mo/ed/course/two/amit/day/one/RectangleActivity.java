f6a1e649cfa8bf287057a4b4eb07dda542eeb1be
Mohamed Atef <proofesser@gmail.com>
2023-02-23 22:47:20 +0200

    Rectangle layout

---
+++ app/src/main/java/mo/ed/course/two/amit/day/one/RectangleActivity.java
@@ -0,0 +1,14 @@
+package mo.ed.course.two.amit.day.one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RectangleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rectangle);
+    }
+}
