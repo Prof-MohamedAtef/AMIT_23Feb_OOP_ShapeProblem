432fde2216396964c4c12e7bd2fc2bc7922a7bf8
Mohamed Atef <proofesser@gmail.com>
2023-02-23 21:51:30 +0200

    removed ActivityLifeCycle from MainActivity.java

--- app/src/main/java/mo/ed/course/two/amit/day/one/MainActivity.java
+++ app/src/main/java/mo/ed/course/two/amit/day/one/MainActivity.java
@@ -10,63 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    int onCreateCounter=0;
-    int onStartCounter=0;
-    int onResumeCounter=0;
-    int onPauseCounter=0;
-    int onStopCounter=0;
-    int onDestroyCounter=0;
-
-    int onRestartCounter=0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        onCreateCounter++;
-        Log.e("OnCreateLog","OnCreateLog Counter: "+onCreateCounter);
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        onStartCounter++;
-        Log.e("OnStartLog","OnStartLog Counter: "+onStartCounter);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        onResumeCounter++;
-        Log.e("OnResumeLog","OnResumeLog Counter: "+onResumeCounter);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        onPauseCounter++;
-        Log.e("OnPauseLog","OnPauseLog Counter: "+onPauseCounter);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        onStopCounter++;
-        Log.e("OnStopLog","OnStopLog Counter: "+onStopCounter);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        onDestroyCounter++;
-        Log.e("OnDestroyLog","OnDestroyLog Counter: "+onDestroyCounter);
-    }
-
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        onRestartCounter++;
-        Log.e("onRestartLog","onRestartLog Counter: "+onRestartCounter);
     }
 }
