f9c97c16dd18e27d6afb8e8e1d3d7600753858d0
Mohamed Atef <proofesser@gmail.com>
2023-02-23 22:00:44 +0200

    using data binding

--- app/src/main/java/mo/ed/course/two/amit/day/one/MainActivity.java
+++ app/src/main/java/mo/ed/course/two/amit/day/one/MainActivity.java
@@ -7,13 +7,21 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+
+import mo.ed.course.two.amit.day.one.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Double mLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mLength= Double.valueOf(binding.etLength.getText().toString());
 
     }
 }
